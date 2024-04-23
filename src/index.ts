ffb29ac8fba95646be7dd61b7fc9aecedd9a5582
Greg Smalley <gsmalley@gmail.com>
2024-04-22 20:11:39 -0400

    1.1

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,23 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+// src/index.js
+var src_default = {
+  async fetch(request, env, ctx) {
+    if(request.url == "https://www.cloudflareworkers.com/test") {
+           return new Response('Hello worker!', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+       else{
+           return new Response('Error Worker! Wrong URL', {
+               headers: {
+                   'content-type': 'text/plain',
+               },
+           });
+       }
+  }
 };
+export {
+  src_default as default
+};
+//# sourceMappingURL=index.js.map
